bb2af02aefcd52380a4724a8ddae9f7f27d36072
mortinius alamandus local <134599956+mortiniusalamandus@users.noreply.github.com>
2024-11-10 13:11:49 +0100

    style: removed the boilerplate CRA and added some text

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <p>Morten Engerviks hjemmeside. Bygget i React med CRA, local dev. i VS Code, hosted on Vercel og remote repo på Github</p>
+  )
 }
 
 export default App;
